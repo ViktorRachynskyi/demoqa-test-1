d6e81ad92110d536629e68bf7f2351489e846297
Viktor <viktor.rachynskyi@asquad.io>
2022-02-19 13:00:14 +0200

    test in concol

--- src/test/java/junit/FirstJUnitTest.java
+++ src/test/java/junit/FirstJUnitTest.java
@@ -1,11 +1,38 @@
 package junit;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.*;
 
 public class FirstJUnitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+        System.out.println("before all");
+    }
+
+    @BeforeEach
+    void openBrowser() {
+        System.out.println("BeforeEach");
+    }
+
+    @AfterEach
+    void closeBrowser() {
+        System.out.println("AfterEach");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
     @Test
-    void simpleTest() {
-        Assertions.assertTrue(2 < 3);
+    void simple1Test() {
+        System.out.println("test 1");
+    }
+
+    @Test
+    void simple2Test() {
+        System.out.println("test 2");
     }
 }
