4dd61d71f88bf72656d54acb4e3566f3e0dd16d8
Viktor <viktor.rachynskyi@asquad.io>
2022-02-19 12:41:37 +0200

    try add test

--- src/test/java/junit/FirstJUnitTest.java
+++ src/test/java/junit/FirstJUnitTest.java
@@ -1,5 +1,11 @@
 package junit;
 
-public class FirstJUnitTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class FirstJUnitTest {
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(2 < 3);
+    }
 }
